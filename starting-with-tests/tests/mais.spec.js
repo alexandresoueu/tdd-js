9b929cbeb10b4995f655f7b54d098c0e7796ed36
Alexandre DEV <calexandredev@gmail.com>
2019-01-06 19:41:30 -0200

    Starting session test with mocha and chai

---
+++ starting-with-tests/tests/mais.spec.js
@@ -0,0 +1,16 @@
+describe('Main', function() {
+  describe('Method A', function() {
+    context('Case One', function() {
+      it('Should happen ...', function() {
+        
+      })
+    })
+
+    context('Case Two', function() {
+
+    })
+  })
+  describe('Method B', function() {
+
+  })
+})
