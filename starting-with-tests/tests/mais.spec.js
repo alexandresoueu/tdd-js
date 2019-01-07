e17b1e6a1cd3f55a23b2902f29992c4a505e00ac
AlexandreSouEu <calexandredev@gmail.com>
2019-01-07 13:24:14 -0200

    tests using skip, only and --bail

--- starting-with-tests/tests/mais.spec.js
+++ starting-with-tests/tests/mais.spec.js
@@ -4,10 +4,26 @@ describe('Main', function() {
       it('Should happen ...', function() {
         
       })
+      
+      it.skip('Should happen One...', function() {
+        
+        throw new Error('just an error')
+      })
+
+      it('Should happen Two ...', function() {
+        
+      })
     })
 
     context('Case Two', function() {
+      it('Should happen Bubba...', function() {
+        
+        throw new Error('just an error on the code.')
+      })
 
+      it('Should happen Bubba noob...', function() {
+        
+      })
     })
   })
   describe('Method B', function() {
