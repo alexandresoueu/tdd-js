7698863a597971570eb33cc7445be5e3ecc0540e
AlexandreSouEu <calexandredev@gmail.com>
2019-01-09 08:55:30 -0200

    Using hooks inside tests

--- starting-with-tests/tests/mais.spec.js
+++ starting-with-tests/tests/mais.spec.js
@@ -1,26 +1,38 @@
 describe('Main', function() {
-  
+  var arr
   before(function() {
-    console.log('before')
+    
   })
   
   after(function() {
-    console.log('after')
+    
   })
 
   beforeEach(function() {
-    console.log('beforeEach')
+    arr = [1, 2, 3]
   })
 
   afterEach(function()  {
-    console.log('afterEach')
+    
   })
 
-  it('test One', function() {
-    console.log('running test One')
+  it('should have a size of 4 when push another value to the array', function() {
+    console.log('Test 1 before', arr)
+    arr.push(4)
+    console.log(arr.length)
+    console.log('Test 1 after', arr)
   })
 
-  it('test Two', function() {
-    console.log('running test Two')
+  it('should have a size of 2 when pop a value from the array', function() {
+    console.log('Test 2 before', arr)
+    arr.pop()
+    console.log(arr.length)
+    console.log('Test 2 after', arr)
+  })
+
+  it('should remove a value 3 when use pop in the array', function() {
+    console.log('Test 3 before', arr)
+    console.log(arr.pop() === 3)
+    console.log('Test 3 after', arr)
   })
 })
